07e5b562c8e63323401e0870fa515a8ff9e16c56
isnawatinoer <isnawatinew06@gmail.com>
2022-10-24 17:54:41 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello word<br>";
+
+$nama = "Isna";
+$umur = 21;
+
+echo "Nama saya <strong> $nama</strong>, saya berusia  $umur tahun <br>";
+
+$namaAdik = "Rifai";
+$umurAdik= 12;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun<br>";
+
+$selisihUmur = $umur-$umurAdik;
+
+echo "Selisih umur saya dan adik saya $selisihUmur tahun <br/>";
+
+
+?>
